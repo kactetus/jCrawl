c23b5b4012000f6faf4c254ffeb09ba6652bb4cd
jmenicucci <labayah@gmail.com>
2016-02-27 15:30:50 -0700

    Adding crawler

---
+++ src/Library/functions.php
@@ -0,0 +1,345 @@
+<?php
+function truncate($string, $length=100, $append="&hellip;")
+{
+	$string = trim($string);
+	$string = explode(' ', $string);
+	$string = array_map('trim', $string);
+	$string = implode(' ', $string);
+	if(strlen($string) > $length) {
+		$string = wordwrap($string, $length);
+		$string = explode("\n", $string, 2);
+		$string = $string[0] . $append;
+	}
+	return $string;
+}
+
+
+function shorturl($url){
+    $length = STRLEN($url);
+    if($length > 165){
+        $length = $length - 65;
+        $first = SUBSTR($url, 0, -$length);
+        $last = SUBSTR($url, -50);
+        $new = $first."/.../".$last;
+        return $new;
+    }else{
+        return $url;
+    }
+}
+
+function strip_html_tags( $text )
+{
+    $text = preg_replace(
+        array(
+          // Remove invisible content
+            '@<head[^>]*?>.*?</head>@siu',
+            '@<style[^>]*?>.*?</style>@siu',
+            '@<script[^>]*?.*?</script>@siu',
+            '@<object[^>]*?.*?</object>@siu',
+            '@<embed[^>]*?.*?</embed>@siu',
+            '@<applet[^>]*?.*?</applet>@siu',
+            '@<noframes[^>]*?.*?</noframes>@siu',
+            '@<noscript[^>]*?.*?</noscript>@siu',
+            '@<noembed[^>]*?.*?</noembed>@siu',
+          // Add line breaks before and after blocks
+            '@</?((address)|(blockquote)|(center)|(del))@iu',
+            '@</?((div)|(h[1-9])|(ins)|(isindex)|(p)|(pre))@iu',
+            '@</?((dir)|(dl)|(dt)|(dd)|(li)|(menu)|(ol)|(ul))@iu',
+            '@</?((table)|(th)|(td)|(caption))@iu',
+            '@</?((form)|(button)|(fieldset)|(legend)|(input))@iu',
+            '@</?((label)|(select)|(optgroup)|(option)|(textarea))@iu',
+            '@</?((frameset)|(frame)|(iframe))@iu',
+        ),
+        array(
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0",
+            "\n\$0", "\n\$0",
+        ),
+        $text );
+    return strip_tags( $text );
+}
+
+function checkImage($image)
+{
+	return preg_match('/[\w\-]+\.(jpg|png|gif|jpeg)/', $image);
+}
+function checkUrl($url)
+{
+    if(is_array($url)) 
+        $url = $url[0];
+
+	return preg_match('%^((https?://)|(www\.))([a-z0-9-].?)+(:[0-9]+)?(/.*)?$%i', $url);
+}
+function makeObject($object)
+{
+	$object = json_encode($object);
+	$object = json_decode($object);
+	return $object;
+}
+function url_to_absolute( $baseUrl, $relativeUrl )
+{
+    // If relative URL has a scheme, clean path and return.
+    $r = split_url( $relativeUrl );
+    if ( $r === FALSE )
+        return FALSE;
+    if ( !empty( $r['scheme'] ) )
+    {
+        if ( !empty( $r['path'] ) && $r['path'][0] == '/' )
+            $r['path'] = url_remove_dot_segments( $r['path'] );
+        return join_url( $r );
+    }
+ 
+    // Make sure the base URL is absolute.
+    $b = split_url( $baseUrl );
+    if ( $b === FALSE || empty( $b['scheme'] ) || empty( $b['host'] ) )
+        return FALSE;
+    $r['scheme'] = $b['scheme'];
+ 
+    // If relative URL has an authority, clean path and return.
+    if ( isset( $r['host'] ) )
+    {
+        if ( !empty( $r['path'] ) )
+            $r['path'] = url_remove_dot_segments( $r['path'] );
+        return join_url( $r );
+    }
+    unset( $r['port'] );
+    unset( $r['user'] );
+    unset( $r['pass'] );
+ 
+    // Copy base authority.
+    $r['host'] = $b['host'];
+    if ( isset( $b['port'] ) ) $r['port'] = $b['port'];
+    if ( isset( $b['user'] ) ) $r['user'] = $b['user'];
+    if ( isset( $b['pass'] ) ) $r['pass'] = $b['pass'];
+ 
+    // If relative URL has no path, use base path
+    if ( empty( $r['path'] ) )
+    {
+        if ( !empty( $b['path'] ) )
+            $r['path'] = $b['path'];
+        if ( !isset( $r['query'] ) && isset( $b['query'] ) )
+            $r['query'] = $b['query'];
+        return join_url( $r );
+    }
+ 
+    // If relative URL path doesn't start with /, merge with base path
+    if ( $r['path'][0] != '/' )
+    {
+        $base = isset($b['path']) ? mb_strrchr( $b['path'], '/', TRUE, 'UTF-8' ) : FALSE;
+        if ( $base === FALSE ) $base = '';
+        $r['path'] = $base . '/' . $r['path'];
+    }
+    $r['path'] = url_remove_dot_segments( $r['path'] );
+    return join_url( $r );
+}
+function split_url( $url, $decode=TRUE )
+{
+	$parts = '';
+    $xunressub     = 'a-zA-Z\d\-._~\!$&\'()*+,;=';
+    $xpchar        = $xunressub . ':@%';
+    $xscheme       = '([a-zA-Z][a-zA-Z\d+-.]*)';
+    $xuserinfo     = '((['  . $xunressub . '%]*)' .
+                     '(:([' . $xunressub . ':%]*))?)';
+    $xipv4         = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})';
+    $xipv6         = '(\[([a-fA-F\d.:]+)\])';
+    $xhost_name    = '([a-zA-Z\d-.%]+)';
+    $xhost         = '(' . $xhost_name . '|' . $xipv4 . '|' . $xipv6 . ')';
+    $xport         = '(\d*)';
+    $xauthority    = '((' . $xuserinfo . '@)?' . $xhost .
+                     '?(:' . $xport . ')?)';
+    $xslash_seg    = '(/[' . $xpchar . ']*)';
+    $xpath_authabs = '((//' . $xauthority . ')((/[' . $xpchar . ']*)*))';
+    $xpath_rel     = '([' . $xpchar . ']+' . $xslash_seg . '*)';
+    $xpath_abs     = '(/(' . $xpath_rel . ')?)';
+    $xapath        = '(' . $xpath_authabs . '|' . $xpath_abs .
+                     '|' . $xpath_rel . ')';
+    $xqueryfrag    = '([' . $xpchar . '/?' . ']*)';
+    $xurl          = '^(' . $xscheme . ':)?' .  $xapath . '?' .
+                     '(\?' . $xqueryfrag . ')?(#' . $xqueryfrag . ')?$';
+ 
+ 
+    // Split the URL into components.
+    if ( !preg_match( '!' . $xurl . '!', $url, $m ) )
+        return FALSE;
+ 
+    if ( !empty($m[2]) )        $parts['scheme']  = strtolower($m[2]);
+ 
+    if ( !empty($m[7]) ) {
+        if ( isset( $m[9] ) )   $parts['user']    = $m[9];
+        else            $parts['user']    = '';
+    }
+    if ( !empty($m[10]) )       $parts['pass']    = $m[11];
+ 
+    if ( !empty($m[13]) )       $h=$parts['host'] = $m[13];
+    else if ( !empty($m[14]) )  $parts['host']    = $m[14];
+    else if ( !empty($m[16]) )  $parts['host']    = $m[16];
+    else if ( !empty( $m[5] ) ) $parts['host']    = '';
+    if ( !empty($m[17]) )       $parts['port']    = $m[18];
+ 
+    if ( !empty($m[19]) )       $parts['path']    = $m[19];
+    else if ( !empty($m[21]) )  $parts['path']    = $m[21];
+    else if ( !empty($m[25]) )  $parts['path']    = $m[25];
+ 
+    if ( !empty($m[27]) )       $parts['query']   = $m[28];
+    if ( !empty($m[29]) )       $parts['fragment']= $m[30];
+ 
+    if ( !$decode )
+        return $parts;
+    if ( !empty($parts['user']) )
+        $parts['user']     = rawurldecode( $parts['user'] );
+    if ( !empty($parts['pass']) )
+        $parts['pass']     = rawurldecode( $parts['pass'] );
+    if ( !empty($parts['path']) )
+        $parts['path']     = rawurldecode( $parts['path'] );
+    if ( isset($h) )
+        $parts['host']     = rawurldecode( $parts['host'] );
+    if ( !empty($parts['query']) )
+        $parts['query']    = rawurldecode( $parts['query'] );
+    if ( !empty($parts['fragment']) )
+        $parts['fragment'] = rawurldecode( $parts['fragment'] );
+    return $parts;
+}
+function join_url( $parts, $encode=TRUE )
+{
+    if ( $encode )
+    {
+        if ( isset( $parts['user'] ) )
+            $parts['user']     = rawurlencode( $parts['user'] );
+        if ( isset( $parts['pass'] ) )
+            $parts['pass']     = rawurlencode( $parts['pass'] );
+        if ( isset( $parts['host'] ) &&
+            !preg_match( '!^(\[[\da-f.:]+\]])|([\da-f.:]+)$!ui', $parts['host'] ) )
+            $parts['host']     = rawurlencode( $parts['host'] );
+        if ( !empty( $parts['path'] ) )
+            $parts['path']     = preg_replace( '!%2F!ui', '/',
+                rawurlencode( $parts['path'] ) );
+        if ( isset( $parts['query'] ) )
+            $parts['query']    = rawurlencode( $parts['query'] );
+        if ( isset( $parts['fragment'] ) )
+            $parts['fragment'] = rawurlencode( $parts['fragment'] );
+    }
+ 
+    $url = '';
+    if ( !empty( $parts['scheme'] ) )
+        $url .= $parts['scheme'] . ':';
+    if ( isset( $parts['host'] ) )
+    {
+        $url .= '//';
+        if ( isset( $parts['user'] ) )
+        {
+            $url .= $parts['user'];
+            if ( isset( $parts['pass'] ) )
+                $url .= ':' . $parts['pass'];
+            $url .= '@';
+        }
+        if ( preg_match( '!^[\da-f]*:[\da-f.:]+$!ui', $parts['host'] ) )
+            $url .= '[' . $parts['host'] . ']'; // IPv6
+        else
+            $url .= $parts['host'];             // IPv4 or name
+        if ( isset( $parts['port'] ) )
+            $url .= ':' . $parts['port'];
+        if ( !empty( $parts['path'] ) && $parts['path'][0] != '/' )
+            $url .= '/';
+    }
+    if ( !empty( $parts['path'] ) )
+        $url .= $parts['path'];
+    if ( isset( $parts['query'] ) )
+        $url .= '?' . $parts['query'];
+    if ( isset( $parts['fragment'] ) )
+        $url .= '#' . $parts['fragment'];
+    return $url;
+}
+function url_remove_dot_segments( $path )
+{
+    // multi-byte character explode
+    $inSegs  = preg_split( '!/!u', $path );
+    $outSegs = array( );
+    foreach ( $inSegs as $seg )
+    {
+        if ( $seg == '' || $seg == '.')
+            continue;
+        if ( $seg == '..' )
+            array_pop( $outSegs );
+        else
+            array_push( $outSegs, $seg );
+    }
+    $outPath = implode( '/', $outSegs );
+    if ( $path[0] == '/' )
+        $outPath = '/' . $outPath;
+    // compare last multi-byte character against '/'
+    if ( $outPath != '/' &&
+        (mb_strlen($path)-1) == mb_strrpos( $path, '/', 'UTF-8' ) )
+        $outPath .= '/';
+    return $outPath;
+}
+function get_http_response_code($url) {
+    @$headers = get_headers($url);
+    return substr($headers[0], 9, 3);
+}
+function get_http_headers($url) {
+    @$headers = get_headers($url, 1);
+    return $headers;
+}
+
+function fetch($url)
+{
+    // 1. initialize
+    $ch = curl_init();
+    $agent = [
+         "User-Agent: reaver-dirge",
+         "Accept-Language: en-us"
+    ];
+     
+    // 2. set the options, including the url
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $agent);
+     
+    // 3. execute and fetch the resulting HTML output
+    $output = curl_exec($ch);
+
+    if(preg_match("!Location: (.*)!", $output, $matches)) 
+        $redirect = $matches[1];
+    else 
+        $redirect = false;
+     
+     $info = curl_getinfo($ch);
+    // 4. free up the curl handle
+    curl_close($ch);
+
+    return [
+        'site' => $output, 
+        'redirect' => $redirect,
+        'info' => $info
+    ];
+}
+
+function removeElementsByTagName($tagName, $document) {
+  $nodeList = $document->getElementsByTagName($tagName);
+  for ($nodeIdx = $nodeList->length; --$nodeIdx >= 0; ) {
+    $node = $nodeList->item($nodeIdx);
+    $node->parentNode->removeChild($node);
+  }
+}
+
+function highlight($text, $words) {
+    preg_match_all('~\w+~', $words, $m);
+    if(!$m)
+        return $text;
+    $re = '~\\b(' . implode('|', $m[0]) . ')\\b~i';
+    return preg_replace($re, '<b>$0</b>', $text);
+}
+
+
+function word_search($text, $words, $numWordsToWrap, $wordToFind)
+{
+    if (($pos = array_search($wordToFind, $words)) !== FALSE) {
+      $start = ($pos - $numWordsToWrap > 0) ? $pos - $numWordsToWrap : 0;
+      $length = (($pos + ($numWordsToWrap + 1) < count($words)) ? $pos + ($numWordsToWrap + 1) : count($words) - 1) - $start;
+      $slice = array_slice($words, $start, $length);
+      $out = implode(' ', $slice);
+      $text = $out;
+    } 
+    return $text;
+}
